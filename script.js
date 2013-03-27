55018897e6c4c02f2e67e1bf2c8e0a4950c7c3d5
Nellit <light_nata@mail.ru>
2013-03-27 18:41:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var Service_url = "http://localhost/Kurs_5/Experiment_4_kurs/Service/";
+var Service_url = "http://nellit-tests.comxa.com/Service/";
 function get_survey_form(input_id,div_id)
 {
-  //var xhr = new XMLHttpRequest();
-	//var form_id = document.getElementById(input_id).value
-	//xhr.open("POST",Service_url,false);
-	//xhr.setRequestHeader("Content-type","text/xml");
-	//xhr.send("<request action='get_form'><id>"+form_id+"</id></request>");
-	//resText = xhr.responseText
+  var xhr = new XMLHttpRequest();
+	var form_id = document.getElementById(input_id).value
+	xhr.open("POST",Service_url,false);
+	xhr.setRequestHeader("Content-type","text/xml");
+	xhr.send("<request action='get_form'><id>"+form_id+"</id></request>");
+	resText = xhr.responseText
 	//alert(resText);
-	document.getElementById(div_id).innerHTML = "HelloWorld!";//resText;
+	document.getElementById(div_id).innerHTML = resText;
 }
