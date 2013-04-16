9016089b30eaba97885144ef697a1d3f0a0263c7
Nellit <light_nata@mail.ru>
2013-04-16 10:33:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 var Service_url = "http://nellit-tests.comxa.com/Service/";
+function get_server_answer(event,div_id)
+{
+	if (window.FormData) 
+	{
+		var Form = document.getElementById('ssform');
+		event.preventDefault();
+		var data = new FormData(Form);
+		//alert(data);
+		var xhr = new XMLHttpRequest();
+		var url = Form.getAttribute('action');
+		xhr.open('post', url);
+		xhr.send(data);
+		xhr.onreadystatechange = function() 
+		{	
+			if (xhr.readyState == 4 && xhr.status == 200) 
+			{				
+			resText = xhr.responseText;
+			document.getElementById(div_id).innerHTML = resText;
+			}		
+		}
+	};
+	return false;
+};
+
 function get_survey_form(input_id,div_id)
 {
-  var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
 	var form_id = document.getElementById(input_id).value
 	xhr.open("POST",Service_url,false);
 	xhr.setRequestHeader("Content-type","text/xml");
@@ -9,4 +33,6 @@ function get_survey_form(input_id,div_id)
 	resText = xhr.responseText
 	//alert(resText);
 	document.getElementById(div_id).innerHTML = resText;
+	var Form = document.getElementById('ssform');
+	Form.setAttribute('onsubmit', 'get_server_answer(event,\''+div_id+'\');')
 }
